93a137ec74a1f4d07ef7ac9dfa44ac54918dc012
okquentin <qosterhage@outlook.com>
2023-11-03 09:49:30 -0400

    Finished W11 Demo
    Express project that displays a table on a locally-hosted website (port 3000)

--- W11/week11a_demo/routes/index.js
+++ W11/week11a_demo/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let schedule = [
+    {week: 10, dow: "Monday", topic: "Mini Midterm 2"},
+    {week: 10, dow: "Wednesday", topic: "Model-view-controller"},
+    {week: 10, dow: "Friday", topic: "Templating with Pug"},
+    {week: 11, dow: "Monday", topic: "Introduction to Express and NPM"},
+    {week: 12, dow: "Wednesday", topic: "MVN architecture of Express"},
+    {week: 12, dow: "Wednesday", topic: "Writing Pug in Express"}
+  ];
+  res.render('index', { title: 'My Website', schedule: schedule });
 });
 
 module.exports = router;
