bdcf6c51ec7825475de747eafe29fccf0efea853
okquentin <qosterhage@outlook.com>
2023-12-11 13:44:29 -0500

    Added pictures and standardized some formatting

--- FINAL/controllers/scheduleController.js
+++ FINAL/controllers/scheduleController.js
@@ -5,15 +5,22 @@ const asyncHandler = require('express-async-handler')
 // Display list of all days of the schedule.
 exports.schedule_list = asyncHandler(async (req, res, next) => {
 
-  let fullSchedule = await Schedule.find({}, "week dow topic").exec();
+  let fullSchedule = await Schedule.find({}, "week dow topic").sort('week').exec();
+
+  const dowOrder = ['monday', 'wednesday', 'friday']
+
+  fullSchedule.sort((a, b) => {
+    if (a.week === b.week) {
+      return dowOrder.indexOf(a.dow.toLowerCase()) - dowOrder.indexOf(b.dow.toLowerCase());
+    }
+    return 0;
+  });
 
   fullSchedule = fullSchedule.map((obj) => {
     // TODO any pre-processing of the schedule data 
-
     return obj;
   })
 
-  res.render('schedule', {"schedule": fullSchedule});
-
+  res.render('schedule', {title: 'Schedule', activePage: 'schedule', "schedule": fullSchedule});
 });
 
